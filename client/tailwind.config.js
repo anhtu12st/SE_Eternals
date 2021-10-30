5d72f41d7578a6ec027b0dd3f7c36e467e23827f
tuandat <huynhtuandat05december@gmail.com>
2021-10-31 05:49:39 +0700

    Setup color and package icon

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-1': '#EEEDF3',
+        'white-2': '#BBB8C4',
+        'white-3': '#5F5C5E',
+        'brown-1': '#27110C',
+        'brown-2': '#A7775C',
+
+      }
+    },
   },
   variants: {
     extend: {},
