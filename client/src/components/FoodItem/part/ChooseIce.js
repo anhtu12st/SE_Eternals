c1c830bb538f28dc16472441a2cd1f20f9486256
tranviet <tranquocviet26701@gmail.com>
2021-11-04 09:52:51 +0700

    Fixed checked option item

--- client/src/components/FoodItem/part/ChooseIce.js
+++ client/src/components/FoodItem/part/ChooseIce.js
@@ -2,17 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ChooseIce(props) {
-  const { ice, onChangeOption } = props;
+  const { id, ice, onChangeOption } = props;
 
   return (
     <div className="py-3">
       <div className="pl-1 font-bold">Ice</div>
-      <label htmlFor="ice_30" className="inline-block p-1">
+      <label htmlFor={`${id}-ice_30`} className="inline-block p-1">
         <input
           type="radio"
           name="ice"
           className="ice-input hidden"
-          id="ice_30"
+          id={`${id}-ice_30`}
+          value="ice_30"
           onChange={onChangeOption}
         />
         <div className={`
@@ -23,12 +24,13 @@ function ChooseIce(props) {
           <div className="font-bold text-xs text-yellow-500">30%</div>
         </div>
       </label>
-      <label htmlFor="ice_50" className="inline-block p-1">
+      <label htmlFor={`${id}-ice_50`} className="inline-block p-1">
         <input
           type="radio"
           name="ice"
           className="ice-input hidden"
-          id="ice_50"
+          id={`${id}-ice_50`}
+          value="ice_50"
           onChange={onChangeOption}
         />
         <div className={`
@@ -39,12 +41,13 @@ function ChooseIce(props) {
           <div className="font-bold text-xs text-brown-1">50%</div>
         </div>
       </label>
-      <label htmlFor="ice_70" className="inline-block p-1">
+      <label htmlFor={`${id}-ice_70`} className="inline-block p-1">
         <input
           type="radio"
           name="ice"
           className="ice-input hidden"
-          id="ice_70"
+          id={`${id}-ice_70`}
+          value="ice_70"
           onChange={onChangeOption}
         />
         <div className={`
@@ -60,6 +63,7 @@ function ChooseIce(props) {
 }
 
 ChooseIce.propTypes = {
+  id: PropTypes.number.isRequired,
   ice: PropTypes.string,
   onChangeOption: PropTypes.func,
 };
