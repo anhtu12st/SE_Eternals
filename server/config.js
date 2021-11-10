774b3abb97460233f52b33d4d9c13dfe6a6c0777
anhtu12st <anhtu12st@gmail.com>
2021-11-10 11:34:25 +0700

    change script cmd

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  port: 8080,
+  port: process.env.PORT || 8080,
   mongoUrl: "mongodb+srv://se_eternals:DxeD3HmgNRLQ3NqC@cluster0.8ms8x.mongodb.net/pos-test?retryWrites=true&w=majority",
 };
 
